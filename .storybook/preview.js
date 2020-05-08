2f795722639c7088fb0ff2ae43c06caeb11d7e11
Stuart Hendren <stuart@committed.io>
2020-05-08 13:30:29 +0100

    sp fix in preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,10 @@
-
 import React from 'react'
 import { addParameters } from '@storybook/react'
 import committedTheme from './theme.js'
 
 addParameters({
   options: {
-    // Currently require here aswell as in manager.js - should be able to remove in 6.0
+    // Currently required here as well as in manager.js - should be able to remove in 6.0
     theme: committedTheme,
-  }
+  },
 })
